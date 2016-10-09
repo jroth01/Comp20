13386cb429a55d3a7d9cc6e657ed59e85b39e99c
jroth01 <justincarlroth@gmail.com>
2016-10-09 18:56:38 -0400

    Added markers for each red line station

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -13,6 +13,124 @@ var map;
 var marker;
 var infowindow = new google.maps.InfoWindow();
 
+var stations = [
+{
+    name: "South Station",
+    lat: 42.352271,
+    lg: -71.05524200000001
+},
+{
+    name: "Andrew",
+    lat: 42.330154,
+    lg: -71.057655
+},
+{
+    name: "Porter Square",
+    lat: 42.3884,
+    lg: -71.11914899999999
+},
+{
+    name: "Harvard Square",
+    lat: 42.373362,
+    lg: -71.118956
+},
+{
+    name: "JFK/UMass",
+    lat: 42.320685, 
+    lg: -71.052391
+},
+{
+    name: "Savin Hill",
+    lat: 42.31129, 
+    lg: -71.053331
+},
+{
+    name: "Savin Hill",
+    lat: 42.31129, 
+    lg: -71.053331
+},
+{
+    name: "Park Street",
+    lat: 42.35639457, 
+    lg: -71.0624242
+},
+{
+    name: "Broadway",
+    lat: 42.342622, 
+    lg: -71.056967
+},
+{
+    name: "North Quincy",
+    lat: 42.275275, 
+    lg: -71.029583
+},
+{
+    name: "Shamut",
+    lat: 42.29312583, 
+    lg: -71.06573796000001
+},
+{
+    name: "Davis",
+    lat: 42.39674, 
+    lg: -71.121815
+},
+{
+    name: "Alewife",
+    lat: 42.395428, 
+    lg: -71.142483
+},
+{
+    name: "Kendall/MIT",
+    lat: 42.36249079, 
+    lg: -71.08617653
+},
+{
+    name: "Charles/MGH",
+    lat: 42.361166, 
+    lg: -71.070628
+},
+{
+    name: "Downtown Crossing",
+    lat: 42.355518, 
+    lg: -71.060225
+},
+{
+    name: "Quincy Center",
+    lat:  42.251809, 
+    lg: -71.005409
+},
+{
+    name: "Quincy Adams",
+    lat: 42.233391, 
+    lg: -71.007153
+},
+{
+    name: "Ashmont",
+    lat: 42.284652, 
+    lg: -71.06448899999999
+},
+{
+    name: "Wollaston",
+    lat: 42.2665139, 
+    lg: -71.0203369
+},
+{
+    name: "Fields Corner",
+    lat: 42.300093, 
+    lg: -71.061667
+},
+{
+    name: "Central Square",
+    lat: 42.365486, 
+    lg: -71.103802
+},
+{
+    name: "Braintree",
+    lat: 42.2078543, 
+    lg: -71.0011385
+}
+];
+
 //http://stackoverflow.com/questions/30008114/how-do-i-promisify-native-xhr
 function httpRequest(method, url) {
         //returns a promise
@@ -41,15 +159,35 @@ function queryTripList()
 function parseTripList()
 {      
         // get triplist JSON
-        console.log(mbtaTrips.TripList.Trips);
-       
+         mbtaTrips = mbtaTrips.TripList.Trips
+        var lt, lg, trainName;
+        console.log(mbtaTrips);
+        for (var i = 0; i < mbtaTrips.length; i++) {
+            lt = mbtaTrips[i].Position.Lat;
+            lg = mbtaTrips[i].Position.Long;
+            trainName = mbtaTrips[i].Position.Train;
+            console.log(trainName);
+            createMarker(lt,lg, trainName, "train.png");
+        }
 }
 
 function init()
 {
         map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
         getMyLocation();
+        plotStations();
         queryTripList();
+}
+
+function plotStations() 
+{
+    var lt, lg, name;
+    for (var i = 0; i < stations.length; i++) {
+        name = stations[i].name;
+        lt = stations[i].lat;
+        lg = stations[i].lg;
+         createMarker(lt,lg, name, "mbta.png");
+    }
 }
 
 function getMyLocation() {
@@ -65,21 +203,16 @@ function getMyLocation() {
         }
 }
 
-// takes in a cm function 
-function markStops(cm,foo)
-{
-       for (i = 0; i < stops.length; i++) {
-
-       }
-}
-
-function createMarker(position, title) {
+function createMarker(lat,lg,str, imgUrl) {
         // Create a marker
+        var train = new google.maps.LatLng(lat, lg);
         marker = new google.maps.Marker({
-                position: me,
-                title: "Here I Am!"
+                position: train,
+                title: str,
+                icon: imgUrl
         });
         marker.setMap(map);
+
 }
 
 function renderMap()
